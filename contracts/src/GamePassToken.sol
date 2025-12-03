6646c982521b9a1b9ed4d659e68d04a74a4ed3da
dev.uche <uchennahanson@gmail.com>
2025-12-03 18:26:54 +0100

    feat: add access control and security imports

--- contracts/src/GamePassToken.sol
+++ contracts/src/GamePassToken.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
