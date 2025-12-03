8491579b598ba8268c8777f5a58892e47b6a9174
ogazboiz <ogazboizakpolo@gmail.com>
2025-12-03 15:46:17 +0100

    test: add test setup and basic structure

--- contracts/test/GamePassGem.t.sol
+++ contracts/test/GamePassGem.t.sol
@@ -5,10 +5,35 @@ import {Test, console} from "forge-std/Test.sol";
 import {GamePassGem} from "../src/GamePassGem.sol";
 
 contract GamePassGemTest is Test {
-    GamePassGem gem;
+    GamePassGem public gem;
+    
+    address public owner = address(1);
+    address public treasury = address(2);
+    address public player1 = address(3);
+    address public paymentToken = address(4); // Mock token address
+    
+    uint256 constant PRICE_PER_GEM = 34 * 10**18;
+    uint256 constant MAX_SUPPLY = 10000;
     
     function setUp() public {
-        // Setup will be added in next commit
+        vm.startPrank(owner);
+        
+        gem = new GamePassGem(
+            "GamePass Gem",
+            "GPG",
+            MAX_SUPPLY,
+            paymentToken,
+            treasury
+        );
+        
+        gem.setClaimConditions(true, block.timestamp);
+        
+        vm.stopPrank();
+    }
+    
+    function testMintGem() public {
+        // Test will be implemented after token contract is created
+        assertTrue(true);
     }
 }
 
