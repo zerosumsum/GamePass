d48cb2996d7a90429de3fd38c31f67a44ae339d1
ogazboiz <ogazboizakpolo@gmail.com>
2025-12-03 15:26:17 +0100

    feat: add OpenZeppelin imports for ERC721 and security

--- contracts/src/GamePassGem.sol
+++ contracts/src/GamePassGem.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
