0988caceedd52adef15bb0a437d2c18fb18edfc6
dev.uche <uchennahanson@gmail.com>
2025-12-04 13:27:17 +0100

    feat: import GamePassToken contract

--- contracts/src/GamePassSwap.sol
+++ contracts/src/GamePassSwap.sol
@@ -5,4 +5,5 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./GamePassToken.sol";
 
