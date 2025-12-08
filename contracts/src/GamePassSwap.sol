d537f8c7c405e7469c4d8b846ae3ace0d0b62806
thebabalola <t.babalolajoseph@gmail.com>
2025-12-08 03:25:01 +0100

    Fix withdrawCELO to use Address.sendValue for safer ETH transfer

--- contracts/src/GamePassSwap.sol
+++ contracts/src/GamePassSwap.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./GamePassToken.sol";
@@ -21,6 +22,7 @@ import "./GamePassToken.sol";
  */
 contract GamePassSwap is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
+    using Address for address payable;
     
     /// @dev GamePassToken contract instance
     GamePassToken public gamePassToken;
@@ -158,7 +160,7 @@ contract GamePassSwap is Ownable, ReentrancyGuard {
      * @dev Withdraw CELO from contract (only owner)
      */
     function withdrawCELO() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+        Address.sendValue(payable(owner()), address(this).balance);
     }
     
     /**
